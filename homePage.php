d07a0d9377c9c65fa1a01cac3a727f28f76985af
labibuddin18 <tokomanfaat6@gmail.com>
2025-11-15 08:42:52 +0700

    perubahan

--- homePage.php
+++ homePage.php
@@ -2,7 +2,7 @@
 	<h1>Sekolah Inklusi</h1>
 	<div class="isi">
 
-		<p>SD Inklusi merupakan sekolah yang menerapkan pendidikan inklusif, 
+		<p>Sekolah Inklusi merupakan sekolah yang menerapkan pendidikan inklusif, 
 			di mana setiap anak baik anak reguler maupun anak berkebutuhan khusus memiliki hak yang sama 
 			untuk mendapatkan pendidikan yang layak, bermutu, dan ramah terhadap keberagaman.</p><br>
 		<p>Sekolah ini berkomitmen untuk menciptakan lingkungan belajar yang inklusif, aman, dan menyenangkan. 
