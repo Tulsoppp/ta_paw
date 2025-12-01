e75237487579aa53e7216fa5d083ba4ad6684a5e
Tulsoppp <alanjrx521@gmail.com>
2025-12-01 15:34:47 +0700

    konfirmasi

---
+++ admin/kl_admin.php
@@ -0,0 +1,6 @@
+<?php
+    require_once 'cekLoginAdmin.php';
+    require_once '../database.php';
+    require_once '../includes/header.php';
+    require_once '../includes/navbarAdmin.php';
+?>
---
+++ siswa/kl_siswa.php
@@ -0,0 +1,6 @@
+<?php
+    require_once 'cekLoginSiswa.php';
+    require_once '../database.php';
+    require_once '../includes/header.php';
+    require_once '../includes/navbarSiswa.php';
+?>
