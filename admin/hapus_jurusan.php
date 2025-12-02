1ae12a9c41081325bb9a52268ad582ffea63f89f
KULINISME <muhisa0906@gmail.com>
2025-12-02 10:58:43 +0700

    update

--- admin/hapus_jurusan.php
+++ admin/hapus_jurusan.php
@@ -8,7 +8,7 @@ require_once "../database.php";
 
 if (isset($_GET["ID_JURUSAN"])) {
     $id=$_GET["ID_JURUSAN"];
-        $stmnt=$pdo->prepare("DELETE FROM jurusan WHERE ID_JURUSAN=:id");
+        $stmnt=$pdo->prepare("SELECT NAMA_KEBUTUHAN DELETE FROM jurusan WHERE ID_JURUSAN=:id");
         $stmnt->bindValue(':id',$id);
         $stmnt->execute();
     header("Location:jurusan.php");
